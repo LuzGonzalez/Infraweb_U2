151e488e49e7e6b0e9665601b5a5cc66fe893391
Luz <luzalgonzalezro@ittepic.edu.mx>
2018-09-20 16:09:40 +0000

    borrar elemento en array

--- deletElemet_array.rb
+++ deletElemet_array.rb
@@ -1,7 +1,9 @@
 def deletElementArray
 	arreglo=[1,2,3]
+	puts "arreglo original"
 	puts arreglo
         arreglo.pop
+	puts "arreglo despues de eliminar elemento"
 	puts arreglo
 end
 deletElementArray
